9b30d410edafb77bb9cc46612b1b9df281835f46
Matt Ifould <ifouldm@gmail.com>
2021-02-26 00:48:38 +0000

    Project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': [4, 'error']
   },
   overrides: [
     {
